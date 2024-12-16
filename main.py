688a159c571481ae6de1370a16d8eeb0782f47bc
BetinaBiju <betinabiju4@gmail.com>
2024-12-16 09:40:52 +0530

    update

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import eel
 from engine.features import *
+from engine.command import *
 
 eel.init('www')
 
 playAssistantSound()
-os.system('start chrome.exe --app="http://127.0.0.1:8000/www/assets/index.html"')
+os.system('start chrome.exe --app="http://127.0.0.1:8000/www/index.html"')
 eel.start('index.html', mode='chrome', host='127.0.0.1', block=True)
